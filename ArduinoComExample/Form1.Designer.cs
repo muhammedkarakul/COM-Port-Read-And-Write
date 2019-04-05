4ccac74c6fb40a909d1fbbbe861796c6fe4f56a9
unknown <muhammedkarakul@gmail.com>
2019-04-05 18:10:21 +0300

    Initial commit

---
+++ ArduinoComExample/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace ArduinoComExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.recivedDataRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.sendButton = new System.Windows.Forms.Button();
+            this.dataTextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.recivedDataLabel = new System.Windows.Forms.Label();
+            this.connectionStateLabel = new System.Windows.Forms.Label();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.connectionButton = new System.Windows.Forms.Button();
+            this.baudRateComboBox = new System.Windows.Forms.ComboBox();
+            this.comPortComboBox = new System.Windows.Forms.ComboBox();
+            this.baudRateLabel = new System.Windows.Forms.Label();
+            this.comLabel = new System.Windows.Forms.Label();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.recivedDataRichTextBox);
+            this.groupBox1.Controls.Add(this.sendButton);
+            this.groupBox1.Controls.Add(this.dataTextBox);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.recivedDataLabel);
+            this.groupBox1.Controls.Add(this.connectionStateLabel);
+            this.groupBox1.Controls.Add(this.clearButton);
+            this.groupBox1.Controls.Add(this.connectionButton);
+            this.groupBox1.Controls.Add(this.baudRateComboBox);
+            this.groupBox1.Controls.Add(this.comPortComboBox);
+            this.groupBox1.Controls.Add(this.baudRateLabel);
+            this.groupBox1.Controls.Add(this.comLabel);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(679, 858);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Control Panel";
+            // 
+            // recivedDataRichTextBox
+            // 
+            this.recivedDataRichTextBox.Location = new System.Drawing.Point(11, 211);
+            this.recivedDataRichTextBox.Name = "recivedDataRichTextBox";
+            this.recivedDataRichTextBox.Size = new System.Drawing.Size(657, 581);
+            this.recivedDataRichTextBox.TabIndex = 13;
+            this.recivedDataRichTextBox.Text = "";
+            // 
+            // sendButton
+            // 
+            this.sendButton.Location = new System.Drawing.Point(450, 798);
+            this.sendButton.Name = "sendButton";
+            this.sendButton.Size = new System.Drawing.Size(218, 50);
+            this.sendButton.TabIndex = 12;
+            this.sendButton.Text = "Sent";
+            this.sendButton.UseVisualStyleBackColor = true;
+            this.sendButton.Click += new System.EventHandler(this.sendButton_Click);
+            // 
+            // dataTextBox
+            // 
+            this.dataTextBox.Location = new System.Drawing.Point(11, 817);
+            this.dataTextBox.Name = "dataTextBox";
+            this.dataTextBox.Size = new System.Drawing.Size(433, 26);
+            this.dataTextBox.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(11, 794);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(119, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Data to be sent";
+            // 
+            // recivedDataLabel
+            // 
+            this.recivedDataLabel.AutoSize = true;
+            this.recivedDataLabel.Location = new System.Drawing.Point(11, 188);
+            this.recivedDataLabel.Name = "recivedDataLabel";
+            this.recivedDataLabel.Size = new System.Drawing.Size(105, 20);
+            this.recivedDataLabel.TabIndex = 8;
+            this.recivedDataLabel.Text = "Recived Data";
+            // 
+            // connectionStateLabel
+            // 
+            this.connectionStateLabel.AutoSize = true;
+            this.connectionStateLabel.Location = new System.Drawing.Point(11, 132);
+            this.connectionStateLabel.Name = "connectionStateLabel";
+            this.connectionStateLabel.Size = new System.Drawing.Size(251, 20);
+            this.connectionStateLabel.TabIndex = 7;
+            this.connectionStateLabel.Text = "Connection Status: No connection";
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(450, 29);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(218, 74);
+            this.clearButton.TabIndex = 5;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // connectionButton
+            // 
+            this.connectionButton.Location = new System.Drawing.Point(226, 29);
+            this.connectionButton.Name = "connectionButton";
+            this.connectionButton.Size = new System.Drawing.Size(218, 74);
+            this.connectionButton.TabIndex = 4;
+            this.connectionButton.Text = "Connect";
+            this.connectionButton.UseVisualStyleBackColor = true;
+            this.connectionButton.Click += new System.EventHandler(this.connectionButton_Click);
+            // 
+            // baudRateComboBox
+            // 
+            this.baudRateComboBox.FormattingEnabled = true;
+            this.baudRateComboBox.Location = new System.Drawing.Point(99, 75);
+            this.baudRateComboBox.Name = "baudRateComboBox";
+            this.baudRateComboBox.Size = new System.Drawing.Size(121, 28);
+            this.baudRateComboBox.TabIndex = 3;
+            // 
+            // comPortComboBox
+            // 
+            this.comPortComboBox.FormattingEnabled = true;
+            this.comPortComboBox.Location = new System.Drawing.Point(99, 29);
+            this.comPortComboBox.Name = "comPortComboBox";
+            this.comPortComboBox.Size = new System.Drawing.Size(121, 28);
+            this.comPortComboBox.TabIndex = 2;
+            // 
+            // baudRateLabel
+            // 
+            this.baudRateLabel.AutoSize = true;
+            this.baudRateLabel.Location = new System.Drawing.Point(7, 78);
+            this.baudRateLabel.Name = "baudRateLabel";
+            this.baudRateLabel.Size = new System.Drawing.Size(86, 20);
+            this.baudRateLabel.TabIndex = 1;
+            this.baudRateLabel.Text = "Baud Rate";
+            // 
+            // comLabel
+            // 
+            this.comLabel.AutoSize = true;
+            this.comLabel.Location = new System.Drawing.Point(7, 32);
+            this.comLabel.Name = "comLabel";
+            this.comLabel.Size = new System.Drawing.Size(82, 20);
+            this.comLabel.TabIndex = 0;
+            this.comLabel.Text = " COM Port";
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(706, 880);
+            this.Controls.Add(this.groupBox1);
+            this.MaximumSize = new System.Drawing.Size(728, 936);
+            this.MinimumSize = new System.Drawing.Size(728, 936);
+            this.Name = "Form1";
+            this.Text = "COM Port Read And Write";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button sendButton;
+        private System.Windows.Forms.TextBox dataTextBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label recivedDataLabel;
+        private System.Windows.Forms.Label connectionStateLabel;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button connectionButton;
+        private System.Windows.Forms.ComboBox baudRateComboBox;
+        private System.Windows.Forms.ComboBox comPortComboBox;
+        private System.Windows.Forms.Label baudRateLabel;
+        private System.Windows.Forms.Label comLabel;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.RichTextBox recivedDataRichTextBox;
+    }
+}
+
---
+++ ArduinoComExample/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace ArduinoComExample
+{
+    public partial class Form1 : Form
+    {
+
+        string[] ports = System.IO.Ports.SerialPort.GetPortNames();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            FormClosing += Form1_FormClosing;
+            serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(serialPort1_DataReceived);
+        }
+
+        private void connectionButton_Click(object sender, EventArgs e)
+        {
+            if (connectionButton.Text == "Connect")
+            {
+                connectionButton.Text = "Stop Connection";
+                connectionStateLabel.Text = "Connection Status: " + comPortComboBox.Text + ", " + baudRateComboBox.Text + ", Connected";
+                serialPort1.PortName = comPortComboBox.Text;
+                serialPort1.BaudRate = Convert.ToInt32(baudRateComboBox.Text);
+                serialPort1.Open();
+            }
+            else {
+                connectionButton.Text = "Connect";
+                connectionStateLabel.Text = "Connection Status: No connection";
+                serialPort1.Close();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach (string port in ports) {
+                comPortComboBox.Items.Add(port);
+            }
+
+            // Add Baud Rates to ComboBox
+            baudRateComboBox.Items.Add("300");
+            baudRateComboBox.Items.Add("600");
+            baudRateComboBox.Items.Add("1200");
+            baudRateComboBox.Items.Add("2400");
+            baudRateComboBox.Items.Add("4800");
+            baudRateComboBox.Items.Add("9600");
+            baudRateComboBox.Items.Add("19200");
+            baudRateComboBox.Items.Add("57600");
+            baudRateComboBox.Items.Add("115200");
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e) {
+            if (serialPort1.IsOpen == true) {
+                serialPort1.Close();
+            }
+        }
+
+        private void contextMenuStrip1_Opening(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            recivedDataRichTextBox.Clear();
+        }
+
+        private void sendButton_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen == true)
+            {
+                serialPort1.Write(dataTextBox.Text + "\n");
+            }
+            else {
+                MessageBox.Show("You cannot send data without a connection!");
+            }
+        }
+
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            Thread.Sleep(30);
+
+            try
+            {
+                string data = serialPort1.ReadLine(); // Read data from buffer
+
+                if (recivedDataRichTextBox.InvokeRequired)
+                {
+                    recivedDataRichTextBox.Invoke(new MethodInvoker(delegate { recivedDataRichTextBox.Text += data + "\r\n"; }));
+                }
+            }
+            catch {
+                MessageBox.Show("There was a problem when disconnecting the connection.");
+            }
+
+            
+            
+        }
+    }
+}
